ab0a0ccd8cf435c38523e88b85fb3d07af5ae804
Daniel Scott-Raynsford <plagueho@gmail.com>
2018-03-11 14:03:37 +1300

    Try again

--- psakefile.ps1
+++ psakefile.ps1
@@ -238,16 +238,16 @@ Task Deploy -Depends Build {
 
                 # Pull the master branch, update the readme.md and manifest
                 Set-Location -Path $ProjectRoot
-                exec { git @('config', '--global', 'credential.helper', 'store') }
+                & git @('config', '--global', 'credential.helper', 'store')
 
                 Add-Content `
                     -Path "$env:USERPROFILE\.git-credentials" `
                     -Value "https://$($env:GitHubPushFromPlagueHO):x-oauth-basic@github.com`n"
 
-                exec { git @('config', '--global', 'user.email', 'plagueho@gmail.com') }
-                exec { git @('config', '--global', 'user.name', 'Daniel Scott-Raynsford') }
+                & git @('config', '--global', 'user.email', 'plagueho@gmail.com')
+                & git @('config', '--global', 'user.name', 'Daniel Scott-Raynsford')
 
-                exec { git @('checkout', '-f', 'master') } -ErrorAction Continue
+                & git @('checkout', '-f', 'master')
 
                 # Replace the manifest with the one that was published
                 Copy-Item `
@@ -265,21 +265,21 @@ Task Deploy -Depends Build {
 
                 # Update the master branch
                 'Pushing deployment changes to Master'
-                exec { git @('add', '.') } -ErrorAction Continue
-                exec { git @('commit', '-m', "$NewVersion Deploy!") } -ErrorAction Continue
-                exec { git @('status') } -ErrorAction Continue
-                exec { git @('push', 'origin', 'master') } -ErrorAction Continue
+                & git @('add', '.')
+                & git @('commit', '-m', "$NewVersion Deploy!")
+                & git @('status')
+                & git @('push', 'origin', 'master')
 
                 # Create the version tag and push it
                 "Pushing $newVersion tag to Master"
-                exec { git @('tag', '-a', $newVersion, '-m', $newVersion) } -ErrorAction Continue
-                exec { git @('push', 'origin', $newVersion) } -ErrorAction Continue
+                & git @('tag', '-a', $newVersion, '-m', $newVersion)
+                & git @('push', 'origin', $newVersion)
 
                 # Merge the changes to the Dev branch as well
                 'Pushing deployment changes to Dev'
-                exec { git @('checkout', '-f', 'dev') } -ErrorAction Continue
-                exec { git @('merge', 'master') } -ErrorAction Continue
-                exec { git @('push', 'origin', 'dev') } -ErrorAction Continue
+                & git @('checkout', '-f', 'dev')
+                & git @('merge', 'master')
+                & git @('push', 'origin', 'dev')
             }
         }
     }
