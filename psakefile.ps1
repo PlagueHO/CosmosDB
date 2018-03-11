5bda529e7940de6fc74137c80eff56887ef37f13
Daniel Scott-Raynsford <plagueho@gmail.com>
2018-03-11 14:32:42 +1300

    Try fix up again

--- psakefile.ps1
+++ psakefile.ps1
@@ -238,7 +238,7 @@ Task Deploy -Depends Build {
 
                 # Pull the master branch, update the readme.md and manifest
                 Set-Location -Path $ProjectRoot
-                & git @('config', '--global', 'credential.helper', 'store')
+                exec { git @('config', '--global', 'credential.helper', 'store') }
 
                 Add-Content `
                     -Path "$env:USERPROFILE\.git-credentials" `
@@ -247,7 +247,13 @@ Task Deploy -Depends Build {
                 & git @('config', '--global', 'user.email', 'plagueho@gmail.com')
                 & git @('config', '--global', 'user.name', 'Daniel Scott-Raynsford')
 
-                & git @('checkout', '-f', 'master')
+                try
+                {
+                    exec { git @('checkout', '-f', 'master') }
+                }
+                catch
+                {
+                }
 
                 # Replace the manifest with the one that was published
                 Copy-Item `
