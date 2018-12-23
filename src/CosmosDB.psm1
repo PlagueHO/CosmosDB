cd1519f1acf9606367d73c8a5e81a2d579f0cac3
Daniel Scott-Raynsford <plagueho@gmail.com>
2018-12-24 09:20:31 +1300

    Correct Requires in Module

--- src/CosmosDB.psm1
+++ src/CosmosDB.psm1
@@ -2,8 +2,8 @@
 .EXTERNALHELP CosmosDB-help.xml
 #>
 #Requires -Version 5.1
-#Requires –Modules @{ ModuleName = 'Az.Accounts'; ModuleVersion = '1.0.0'; Guid = '17a2feff-488b-47f9-8729-e2cec094624c' }
-#Requires –Modules @{ ModuleName = 'Az.Resources'; ModuleVersion = '1.0.0'; Guid = '48bb344d-4c24-441e-8ea0-589947784700' }
+#Requires -Modules @{ ModuleName = 'Az.Accounts'; ModuleVersion = '1.0.0'; Guid = '17a2feff-488b-47f9-8729-e2cec094624c' }
+#Requires -Modules @{ ModuleName = 'Az.Resources'; ModuleVersion = '1.0.0'; Guid = '48bb344d-4c24-441e-8ea0-589947784700' }
 
 $moduleRoot = Split-Path `
     -Path $MyInvocation.MyCommand.Path `
